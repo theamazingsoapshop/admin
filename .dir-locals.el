15538f003f1c66813d2558d72e758252c1e6a24f
Pieter Breed <pieter.breed@gmail.com>
2020-08-15 14:20:35 +0200

    dir-locals for shadow/cider

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((cider-default-cljs-repl . shadow)
+         (cider-shadow-default-options . "ui")
+         (cider-jack-in-clojurescript . shadow)
+         (cider-preferred-build-tool . shadow))))
